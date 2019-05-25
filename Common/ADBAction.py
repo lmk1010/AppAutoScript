84051df0b1fd4d049807ebcd7d746ba3adbc15e5
lmk1010 <lmkbnb0@gmail.com>
2019-05-25 22:43:09 +0800

    [UPDATE] qutoutiao upgrade

--- Common/ADBAction.py
+++ Common/ADBAction.py
@@ -3,6 +3,12 @@
 import os
 import sys
 from lxml import etree
+import cv2
+import numpy
+import time
+import datetime
+import random
+import threading
 
 # 作为脚本 1，获取当前设备
 def getDevices():
@@ -18,57 +24,92 @@ def getDevices():
     return devicesList
 
 
+def getDeviceSize(device):
+    deviceSize = str(os.popen("adb -s " + device + " shell wm size").read())
+    return deviceSize.strip()
+
+def lightDevice(devices):
+    # 查看屏幕是否熄灭
+    for device in devices:
+        os.popen("adb -s " + device + " shell input keyevent 26")
+        os.popen("adb -s " + device + " shell input swipe 300 1000 300 500")
+
 # 2，获取当前设备的分辨率以及密度 根据不同的分辨率进行不同的配置模拟xy点击
 def getDevicesInfo(devicesList):
-
+    deviceDict = {}
     for device in devicesList:
         deviceInfo = str(os.popen("adb -s " + device + " shell getprop ro.product.model").read())
         deviceSize = str(os.popen("adb -s " + device + " shell wm size").read())
         deviceDensity = str(os.popen("adb -s " + device + " shell wm density").read())
         # 格式化
         deviceInfo = deviceInfo.strip()
-        deviceSize = deviceSize[deviceSize.find(":")+1:].strip()
+        deviceSize = deviceSize[deviceSize.find(":") + 1:].strip()
         deviceDensity = deviceDensity[deviceDensity.find(":") + 1:].strip()
         print("设备名称：" + str(deviceInfo) + " 设备分辨率：" + str(deviceSize) + " 设备DPI：" + str(deviceDensity))
-
 
 # 3，获取所有第三方应用名称
 
 
-
-# 4，获取当前activities 实际每5s刷新检测
-def getCurrentActivities(devicesList):
-    for device in devicesList:
-        deviceCurrentActivity = str(os.popen("adb -s " + device + " shell dumpsys activity activities | grep mFocusedActivity").read())
-        # 格式化
-        deviceCurrentActivity = deviceCurrentActivity.strip()
-        print("设备："+device+" 当前Activity："+deviceCurrentActivity)
+# 4，获取当前activity 实际每5s刷新检测
+def getCurrentActivity(device):
+    deviceCurrentActivity = str(os.popen("adb -s " + device + " shell dumpsys activity activities ").read())
+    # 格式化
+    deviceCurrentActivity = deviceCurrentActivity.strip()
+    print("设备：" + device + " 当前Activity：" + deviceCurrentActivity)
+    return deviceCurrentActivity
 
 
 # 5，调起activity 目前默认群控
-def startActivity(activityName):
+def startActivity(activityName, devicesList):
     for device in devicesList:
-        deviceInfo = str(os.popen("adb -s " + device + " shell am start -n "+activityName).read())
+        deviceStart = str(os.popen("adb -s " + device + " shell am start -n " + activityName).read())
+        if deviceStart.find("Starting") >= 0:
+            print("设备-"+device + " 开启app成功--")
+        else:
+            print("设备-"+device + " 开启app失败--")
+
 
 # 6，截取当前屏幕 存储 分析
-def getSceenSnapshot():
-    for device in devicesList:
-        result = str(os.popen("adb -s " + device + " exec-out screencap -p > "+device+".png").read())
-        print(result)
+def getSceenSnapshot(device):
+    sceenName = str(random.randint(0, 1000)) + ".png"
+    # result = str(os.popen("adb -s " + device + " exec-out screencap -p > "+sceenName).read())
+    createSnapshot = str(os.popen("adb -s " + device + " shell screencap -p /sdcard/" + sceenName).read())
+    pullSnapshotToPC = str(os.popen("adb -s " + device + " pull /sdcard/" + sceenName).read())
+    print(pullSnapshotToPC)
 
-# 7，获取当前界面的xml布局文件
-def getCurrentActivityXml():
-    if devicesList.__sizeof__()==0:
-        return
-    for device in devicesList:
-        xmlFileName = device+"Layout.xml"
-        createUIXml = str(os.popen("adb -s " + device + " shell uiautomator dump /sdcard/"+xmlFileName).read())
-        pushUIXMLToPC = str(os.popen("adb -s " + device + " pull /sdcard/"+xmlFileName).read())
-        print(pushUIXMLToPC)
-        # 开始解析
-        xmlTree = etree.parse(xmlFileName)
+
+# 7，获取当前界面的xml布局文件 此方法行不通了 改用opencv
+def getCurrentActivityXml(device):
+    xmlFileName = str(random.randint(0, 1000)) + "Layout.xml"
+    print(xmlFileName)
+    createUIXml = str(os.popen("adb -s " + device + " shell uiautomator dump /sdcard/" + xmlFileName).read())
+    pushUIXMLToPC = str(os.popen("adb -s " + device + " pull /sdcard/" + xmlFileName).read())
+    # 开始解析
+    if os.path.exists(xmlFileName) == False:
+       getCurrentActivityXml(device)
+    xmlTree = etree.parse(xmlFileName)
+    return xmlTree
+
+
+# 8，点击指定位置xy的
+def clickXY(device, x, y):
+    clickRes = str(os.popen("adb -s " + device + " shell input tap " + str(x) + " " + str(y)).read())
+    return clickRes
+
+
+# 9，点击指定按键 返回键4 home键3 点亮屏幕224
+def clickSystemKey(key, device):
+    clickRes = str(os.popen("adb -s " + device + " shell input keyevent " + str(key)).read())
+    return clickRes
+
+
+# 10，滑动
+def swipeXY(device, x, y, x1, y1):
+    swipeRes = str(os.popen(
+        "adb -s " + device + " shell input swipe " + str(x) + " " + str(y) + " " + str(x1) + " " + str(y1)).read())
+    return swipeRes
+
 
 if __name__ == '__main__':
-    devicesList = getDevices()
-    getCurrentActivities(devicesList)
-    getCurrentActivityXml()
+    devices = getDevices()
+    lightDevice(devices)
--- Reading/qutoutiao.py
+++ Reading/qutoutiao.py
@@ -3,3 +3,64 @@
 
 import os
 import sys
+import ADBAction
+import time
+import threading
+
+
+def swipe(device):
+    ADBAction.swipeXY(device, 648, 1246, 648, 904)
+
+if __name__ == '__main__':
+    # 1，检测设备
+    devices = ADBAction.getDevices()
+    time.sleep(1)
+    ADBAction.lightDevice(devices)
+    time.sleep(2)
+    ADBAction.getDevicesInfo(devices)
+
+    # 2，打开应用
+    ADBAction.startActivity("com.jifen.qukan/com.jifen.qkbase.main.MainActivity", devices)
+    time.sleep(10)
+
+    # 3，截图匹配
+
+
+    # 4，滑动
+    while 1:
+        for device in devices:
+            for i in range(3):
+                t1 = threading.Thread(target=swipe, args={device})
+                t1.start()
+                print("开始阅读.........")
+                time.sleep(0.1)
+                t1.join()
+
+
+
+
+
+
+
+    # 3，回传xml分析判断当前界面
+    # for device in devices:
+    #     UIXml = ADBAction.getCurrentActivityXml(device)
+    #     # 分析是否有干扰框
+    #     result = UIXml.xpath("//node[@resource-id='com.jifen.qukan:id/t_']")
+    #     if result == []:
+    #         print("没有弹出无用对话框")
+    #     else:
+    #         ADBAction.clickSystemKey(4, device)
+    #
+    #     # ADBAction.getSceenSnapshot(device)
+    #     # 分析是否有奖励框
+    #
+    #     # 开始滑动阅读
+    #     while 1:
+    #         ADBAction.swipeXY(device, 733, 2017, 733, 1098)
+    #         time.sleep(0.2)
+    #
+    #
+    #     # 4，点击指定坐标
+    #     # for device in devices:
+    #     #     ADBAction.clickXY(device, 1032, 947)
